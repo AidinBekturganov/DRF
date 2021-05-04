04068f3aa3aed542ab2b58e53c5fd9407ad86cef
Aidin <bekturganovaidin@gmail.com>
2021-05-04 13:59:36 +0600

    JWT TOKEN AND ADDING REGISTRATION SYSTEM TO REACT

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,20 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('khata.urls', namespace='khata')),
     path('api/', include('khata_api.urls', namespace='khata_api')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 
 ]
 
